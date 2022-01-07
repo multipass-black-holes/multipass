7d8fe707ced9f28ad13a96cf91051c4a8b929f60
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2022-01-07 16:29:21 +0000

    Load GWTC-2

--- multinest/prepare.py
+++ multinest/prepare.py
@@ -1,6 +1,7 @@
 import h5py
 import numpy as np
 import struct
+import os
 
 
 def write_record(fp, typ, content):
@@ -32,4 +33,30 @@ def convert_injection(fi='../o3a_bbhpop_inj_info.hdf', fo='inj.rec'):
         write_record(fp, 'd', dat)
 
 
+def load_gw(base='../all_posterior_samples/', v=2, cm=True):
+    if v == 2:
+        pat = re.compile('GW([\d_]*)_comoving.h5')
+
+    files = [pat.match(i) for i in os.listdir(base)]
+    files = [base + i.group(0) for i in files if i]
+
+    m1 = np.array([])
+    m2 = np.array([])
+    rs = np.array([])
+    ce = np.array([])
+
+    offsets = []
+
+    for fn in files:
+        with h5py.File(fn, 'r') as f:
+            if v == 2:
+                d = f['PublicationSamples/posterior_samples']
+                m1 = np.concatenate((m1, d['mass_1_source']))
+                m2 = np.concatenate((m2, d['mass_2_source']))
+                rs = np.concatenate((rs, d['redshift']))
+                ce = np.concatenate((ce, d['chi_eff']))
+
+            offsets.append(len(m1))
+
+    return np.column_stack((m1, m2, rs, ce)), np.array(offsets)
 
