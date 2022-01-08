6397ac7962667ff6a083478f5afcd559ba3cde35
Yannick Ulrich <yannick.ulrich@durham.ac.uk>
2022-01-08 15:27:29 +0000

    LL file

---
+++ multinest/loglikelihood.f95
@@ -0,0 +1,14 @@
+                          !!!!!!!!!!!!!!!!!!!!!!
+                           MODULE LOGLIKELIHOOD
+                          !!!!!!!!!!!!!!!!!!!!!!
+  use models, only: plp_mf
+  implicit none
+
+contains
+
+  SUBROUTINE TEST
+  END SUBROUTINE TEST
+
+                          !!!!!!!!!!!!!!!!!!!!!!
+                         END MODULE LOGLIKELIHOOD
+                          !!!!!!!!!!!!!!!!!!!!!!
