6a1bb049e2b1171e09294b9d6e339a6ec1288a9d
Yannick Ulrich <yannick.ulrich@unibe.ch>
2023-11-06 11:44:56 +0000

    Added mass function plot

---
+++ multinest/paper-plots/mass-function.py
@@ -0,0 +1,19 @@
+import sys
+sys.path.append("..")
+from analyse import *
+
+samples_plp = loadMC('../plp/long.card', 'plp+plp+trivial+trivial')
+samples_ppisn = loadMC('../ppisn/long', 'ppisn+trivial+trivial')
+
+fig = figure()
+plot_bestfit_m1(samples_plp, 'plp+plp+trivial+trivial', fig=fig, label='PLP')
+plot_bestfit_m1(samples_ppisn, 'ppisn+trivial+trivial', fig=fig, label='PPISN')
+
+legend()
+
+xlim(0,90)
+ylim(2e-5,2e-1)
+
+fig.tight_layout()
+
+fig.savefig("mass-function.pdf")
