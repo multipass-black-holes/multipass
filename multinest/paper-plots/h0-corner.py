bc953feb9cfcda8e90dc788e44cdcc380666b43c
Yannick Ulrich <yannick.ulrich@cern.ch>
2024-02-15 14:05:42 +0100

    Split files

--- multinest/paper-plots/h0-corner.py
+++ multinest/paper-plots/h0-corner.py
@@ -2,23 +2,7 @@ import sys
 sys.path.append("..")
 from analyse import *
 
-samples_plp = loadMC('../plp-h0/long.card', 'plp+plp+planck+trivial')
 samples_ppisn = loadMC('../ppisn-h0/long', 'ppisn+planck+trivial')
-
-fig = figure(figsize=(3,2))
-
-density = samples_plp.get1DDensityGridData('h0').normalize()
-plot(density.x, density.P, label='PLP')
-
-density = samples_ppisn.get1DDensityGridData('h0').normalize()
-plot(density.x, density.P, label='PPISN')
-legend()
-
-gca().set_xlabel(r"$H_0\,/\,{\rm km}\,{\rm s}^{-1}\,{\rm Mpc}^{-1}$")
-fig.tight_layout()
-
-fig.savefig("h0.pdf")
-
 
 # corner plot, H0-mgap
 densityH0 = samples_ppisn.get1DDensityGridData('h0').normalize()
@@ -35,4 +19,4 @@ axs[0,1].set_axis_off()
 axs[0,0].get_yaxis().set_ticks([])
 axs[1,1].get_xaxis().set_ticks([])
 
-fig.savefig("h0-mgap.pdf")
+fig.savefig("h0-corner.pdf")
---
+++ multinest/paper-plots/h0.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append("..")
+from analyse import *
+
+samples_plp = loadMC('../plp-h0/long.card', 'plp+plp+planck+trivial')
+samples_ppisn = loadMC('../ppisn-h0/long', 'ppisn+planck+trivial')
+
+fig = figure(figsize=(3,2))
+
+density = samples_plp.get1DDensityGridData('h0').normalize()
+plot(density.x, density.P, label='PLP')
+
+density = samples_ppisn.get1DDensityGridData('h0').normalize()
+plot(density.x, density.P, label='PPISN')
+legend()
+
+gca().set_xlabel(r"$H_0\,/\,{\rm km}\,{\rm s}^{-1}\,{\rm Mpc}^{-1}$")
+fig.tight_layout()
+
+fig.savefig("h0.pdf")
