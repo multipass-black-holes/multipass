380961df823526b42f9a31a88a55417735c60ffa
Yannick Ulrich <yannick.ulrich@cern.ch>
2024-02-20 15:37:34 +0100

    New mass function plot

--- multinest/paper-plots/mass-function.py
+++ multinest/paper-plots/mass-function.py
@@ -7,9 +7,12 @@ samples_plp = loadMC("../plp/long.card", "plp+plp+trivial+trivial")
 samples_ppisn = loadMC("../ppisn/long", "ppisn+trivial+trivial")
 
 fig = figure()
-plot_bestfit_m1(samples_plp, "plp+plp+trivial+trivial", fig=fig, label="PLP")
-plot_bestfit_m1(samples_ppisn, "ppisn+trivial+trivial", fig=fig, label="PPISN")
+fill_between(*best_fit_band(samples_plp, "plp+plp+trivial+trivial", CL=0.68), alpha=0.2, label="PLP")
+fill_between(*best_fit_band(samples_ppisn, "ppisn+trivial+trivial", prescale=15, CL=0.68), alpha=0.2, label="PPISN")
 
+yscale('log')
+ylabel(r"${\rm d}R/{\rm d}m_1$")
+xlabel("$m_1/M_\odot$")
 legend()
 
 xlim(0, 90)
