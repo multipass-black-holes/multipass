4d8db669a00f4105c9db60dea92075ff22f837c4
Yannick Ulrich <yannick.ulrich@cern.ch>
2024-05-22 11:27:46 +0200

    Updated mass function plot

--- multinest/paper-plots/mass-function.py
+++ multinest/paper-plots/mass-function.py
@@ -6,18 +6,24 @@ from analyse import *
 samples_plp = loadMC("../plp/long.card", "plp+plp+trivial+trivial")
 samples_ppisn = loadMC("../ppisn-fixed-norms-cut/long", "ppisn+trivial+trivial")
 
+m1, yaplp, ybplp, ycplp = best_fit_band_norms(samples_plp, "plp+plp+trivial+trivial", CL=0.68)
+m1, yappisn, ybppisn, ycppisn = best_fit_band_norms(samples_ppisn, "ppisn+trivial+trivial", CL=0.68)
+
 fig = figure()
-fill_between(*best_fit_band(samples_plp, "plp+plp+trivial+trivial", CL=0.68), alpha=0.2, label="PLP")
-fill_between(*best_fit_band(samples_ppisn, "ppisn+trivial+trivial", prescale=2, CL=0.68), alpha=0.2, label="PPISN")
+fill_between(m1, yaplp, ybplp, alpha=0.2, label="PLP")
+plot(m1, ycplp, 'C0')
+fill_between(m1, 1.5e-2*yappisn, 1.5e-2*ybppisn, alpha=0.2, label="PPISN")
+plot(m1, 1.5e-2*ycppisn, 'C1')
 
 yscale('log')
+ylim(2e-10,5e3)
 ylabel(r"${\rm d}R/{\rm d}m_1$")
 xlabel("$m_1/M_\odot$")
 legend()
 
-xlim(0, 100)
-ylim(4e-4, 1.4e-1)
+# mmin,dm,mgap,_,_,_,_,_,_=best_fit(samples_ppisn)
+# axvline(mmin, color='black', linewidth=0.4, zorder=1)
+# axvline(mmin+dm, color='black', linewidth=0.4, zorder=1)
+# axvline(mgap, color='black', linewidth=0.4, zorder=1)
 
-fig.tight_layout()
-
-fig.savefig("mass-function-best-fit.pdf")
+# fig.savefig("mass-function-best-fit.pdf")
