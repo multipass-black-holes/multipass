961c96cfa54b95c069ecf230c18e9f45460d80e5
Yannick Ulrich <yannick.ulrich@cern.ch>
2024-06-08 13:00:26 +0200

    Swapped H0 & Mgap axes

--- results/plots/h0.py
+++ results/plots/h0.py
@@ -11,7 +11,7 @@ densityH0_PLP = samples_plp.get1DDensityGridData('h0').normalize()
 samples_ppisn = loadMC('../ppisn-h0/long', 'ppisn+planck+trivial')
 densityH0 = samples_ppisn.get1DDensityGridData('h0').normalize()
 densityMG = samples_ppisn.get1DDensityGridData('mgap').normalize()
-density=samples_ppisn.get2DDensityGridData('mgap','h0').normalize()
+density=samples_ppisn.get2DDensityGridData('h0','mgap').normalize()
 
 
 fig = figure(figsize=(4,3))
@@ -26,26 +26,28 @@ fig.savefig("h0.pdf")
 
 
 fig, axs = subplots(2,2, sharex='col', sharey='row', gridspec_kw={"hspace": 0, "wspace": 0}, figsize=(4,4))
-axs[1,1].plot(densityH0.P, densityH0.x)
-axs[0,0].plot(densityMG.x, densityMG.P)
+axs[0,0].plot(densityH0.x, densityH0.P)
+axs[1,1].plot(densityMG.P, densityMG.x)
 axs[1,0].contourf(*np.meshgrid(density.x,density.y),density.P, levels=density.getContourLevels((0.95,0.68,1e-10)))
 
-axs[1,0].axhspan(sh0es[0]-sh0es[1], sh0es[0]+sh0es[1], color='C1')
-axs[1,0].axhspan(planck[0]-planck[1], planck[0]+planck[1], color='C2')
+axs[1,0].axvspan(sh0es[0]-sh0es[1], sh0es[0]+sh0es[1], color='C1')
+axs[1,0].axvspan(planck[0]-planck[1], planck[0]+planck[1], color='C2')
 
-axs[1,1].axhspan(sh0es[0]-sh0es[1], sh0es[0]+sh0es[1], color='C1')
-axs[1,1].axhspan(planck[0]-planck[1], planck[0]+planck[1], color='C2')
+axs[0,0].axvspan(sh0es[0]-sh0es[1], sh0es[0]+sh0es[1], color='C1')
+axs[0,0].axvspan(planck[0]-planck[1], planck[0]+planck[1], color='C2')
 axs[0,1].legend(
     [matplotlib.lines.Line2D([0], [0], color=f'C{i}') for i in range(3)],
     [r'${\rm This work}$', r'${\rm SH0ES}$', r'${\rm Planck\ 2018}$'],
     loc='center'
 )
 
-axs[1,0].set_ylabel(r"$H_0\,/\,{\rm km}\,{\rm s}^{-1}\,{\rm Mpc}^{-1}$")
-axs[1,0].set_xlabel(r"$M_{\rm BHMG}\,/\,M_{\odot}$")
+axs[1,0].set_xlabel(r"$H_0\,/\,{\rm km}\,{\rm s}^{-1}\,{\rm Mpc}^{-1}$")
+axs[1,0].set_ylabel(r"$M_{\rm BHMG}\,/\,M_{\odot}$")
 axs[0,1].set_axis_off()
 axs[0,0].get_yaxis().set_ticks([])
 axs[1,1].get_xaxis().set_ticks([])
+axs[1,0].set_xlim(20,150)
+axs[1,0].set_ylim(20,120)
 
 fig.savefig("h0-corner.pdf")
 
